d1cd2c870b11a91bb7c33bd7d7d517973a15f8d2
lelekaspb <bov-1986@yandex.ru>
2022-12-29 23:04:16 +0100

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BusinessCardModule } from './business-card/business-card.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/businesscards'),
-    // MongooseModule.forRoot(
-    //   process.env.DATABASE_URL ||
-    //     'mongodb+srv://dragon:<dragon>@travel-destinations.kjlf6mx.mongodb.net/test',
-    // ),
+    //MongooseModule.forRoot('mongodb://localhost/businesscards'),
+    // MongooseModule.forRoot(process.env.LOCAL_DB_URL),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.LOCAL_DB_URL,
+      }),
+    }),
     BusinessCardModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
